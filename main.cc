c0d6a2fbe9275343d5040f41f0fc680b5bdf8545
Ishika Soni <ishikasoni50@gmail.com>
2023-04-14 21:20:44 -0400

    sending rays into the scene, adding a simple camera and background (have to fix some errors)

--- main.cc
+++ main.cc
@@ -5,17 +5,35 @@ Program: A simple brute-force path tracer
 */
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction()); 
+    auto t = 0.5 * (unit_direction.y() + 1.0); 
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main() {
     
     // Image 
-
     // Width and height of the image that will be outputted 
-    const int image_width = 256; 
-    const int image_height = 256; 
+    const auto aspect_ratio = 16.0 / 9.0; 
+    const int image_width = 400; 
+    const int image_height = static_cast<int>(image_width / aspect_ratio); 
+
+    // Camera 
+
+    auto viewport_height = 2.0; 
+    auto viewport_width = aspect_ratio * viewport_height; 
+    auto focal_length = 1.0; 
+
+    auto origin = point3(0, 0, 0); 
+    auto horizontal = vec3(viewport_width, 0, 0); 
+    auto vertical = vec3(0, viewport_height, 0); 
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length); 
 
     // Render 
 
@@ -32,8 +50,10 @@ int main() {
         // Another loop (nested for loop) that starts with 0 and increments till the 
         // width of the image, 255
         for (int i = 0; i < image_width; ++i) {
-
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color); 
 
             // // R (red), g (green), and b (blue) [blue only stays at 0.25] start to 
