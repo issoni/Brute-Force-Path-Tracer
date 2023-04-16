b168a01de4e35f32a86ca896543824f937cad064
Ishika Soni <ishikasoni50@gmail.com>
2023-04-15 23:57:23 -0400

    sphere class with normal determination

--- hittable.h
+++ hittable.h
@@ -7,6 +7,23 @@ struct hit_record {
     point3 p; 
     vec3 normal; 
     double t; 
+    bool front_face; 
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0; 
+        normal = front_face ? outward_normal :-outward_normal; 
+
+    // bool front_face;
+    // if (dot(ray_direction, outward_normal) > 0.0) {
+    //     // ray is inside the sphere
+    //     normal = -outward_normal;
+    //     front_face = false;
+    // } else {
+    //     // ray is outside the sphere
+    //     normal = outward_normal;
+    //     front_face = true;
+    // }
+    }
 }; 
 
 class hittable {
