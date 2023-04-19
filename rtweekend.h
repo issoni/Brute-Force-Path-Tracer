64ad036013fcc7c42a237a6ca24fd79352017987
Ishika Soni <ishikasoni50@gmail.com>
2023-04-19 19:59:17 -0400

    rtweekend.h documentation

--- rtweekend.h
+++ rtweekend.h
@@ -6,6 +6,8 @@
 #include <memory> 
 #include <cstdlib> 
 
+// General main header file for common useful constants and utility functions 
+
 // Usings 
 
 using std::shared_ptr; 
@@ -19,17 +21,18 @@ const double pi = 3.1415926535897932385;
 
 // Utility Functions
 
+// Returns a conversion of degrees to radians 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0; 
 }
 
+// Returns a random real in [0, 1) 
 inline double random_double() {
-    // Returns a random real is [0, 1). 
     return rand() / (RAND_MAX + 1.0); 
 }
 
+// Returns a random real in [min, max)
 inline double random_double(double min, double max) {
-    // Returns a random real in [min, max).
     return min + (max - min) * random_double(); 
 }
 
